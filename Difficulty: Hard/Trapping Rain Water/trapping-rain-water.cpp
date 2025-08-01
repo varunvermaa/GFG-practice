c20f683c050efa73c17adffba0ca0004d3bb93ef
Varun Verma <155096244+varunvermaa@users.noreply.github.com>
2025-08-01 23:51:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Trapping Rain Water/trapping-rain-water.cpp
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.cpp
@@ -1,65 +1,25 @@
 class Solution {
   public:
-    std::vector<int> nge(std::vector<int> a) {
-        std::stack<int> s;
-        int n = a.size();
-        std::vector<int> b(n);
-        for (int i = n - 1; i >= 0; i--) {
-            while (!s.empty() && a[s.top()] <= a[i]) {
-                s.pop();
-            }
-            if (s.empty()) {
-                b[i] = n;
+    int maxWater(vector<int> &arr) {
+        // code here
+        int w=0,lm=0,rm,l,r;
+        int n=arr.size();
+        rm=n-1;
+        l=1;
+        r=rm-1;
+        while(l<=r){
+            if(arr[lm]>arr[rm]){
+                w+= max(0, arr[rm] - arr[r]);
+                if(arr[r]>arr[rm]) rm=r;
+                
+                r--;
             } else {
-                b[i] = s.top();
+
+                w+= max(0,arr[lm] - arr[l]);
+                if(arr[l]>arr[lm]) lm=l;
+                l++;
             }
-            s.push(i);
         }
-        return b;
-    }
-
-    std::vector<int> pge(std::vector<int> a) {
-        std::stack<int> s;
-        int n = a.size();
-        std::vector<int> b(n);
-        for (int i = 0; i < n; i++) {
-            while (!s.empty() && a[s.top()] <= a[i]) {
-                s.pop();
-            }
-            if (s.empty()) {
-                b[i] = -1;
-            } else {
-                b[i] = s.top();
-            }
-            s.push(i);
-        }
-        return b;
-    }
-
-    int maxWater(vector<int>& arr) {
-        int n = arr.size();
-        if (n <= 2) return 0;
-
-        std::vector<int> left_max(n);
-        std::vector<int> right_max(n);
-
-        left_max[0] = arr[0];
-        for (int i = 1; i < n; i++) {
-            left_max[i] = std::max(left_max[i - 1], arr[i]);
-        }
-
-        right_max[n - 1] = arr[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            right_max[i] = std::max(right_max[i + 1], arr[i]);
-        }
-
-        long long total_water = 0;
-
-        for (int i = 0; i < n; i++) {
-            total_water += std::max(0, std::min(left_max[i], right_max[i]) - arr[i]);
-        }
-
-        return static_cast<int>(total_water);
-        
+        return w;
     }
 };
